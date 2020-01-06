2fe8562aaa045cb6d287089b83596219ea9e8d17
Brayan Nuñez <bnunez@isthmusit.com>
2020-01-06 11:18:06 -0600

    mejoras menu

--- src/app/ui/menu/nav-bar/nav-bar.component.ts
+++ src/app/ui/menu/nav-bar/nav-bar.component.ts
@@ -1,49 +1,59 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Observable} from 'rxjs';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import {map, shareReplay} from 'rxjs/operators';
-import {MatToolbar} from '@angular/material';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { map, shareReplay } from "rxjs/operators";
+import { MatToolbar } from "@angular/material";
 
 /**
  * NavBar for Desktop and mobile environments, management position and redirection to profile
  * route
  */
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.scss"]
 })
 export class NavBarComponent implements OnInit, OnDestroy {
-
-  @ViewChild(MatToolbar, {static: false}) matToolbar: MatToolbar;
+  @ViewChild(MatToolbar, { static: false }) matToolbar: MatToolbar;
   public activeSticky = false;
 
   /**
    * Check if is mobile or desktop device
    */
   isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
+    .observe(["(max-width: 767px)"])
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private router: Router,
-              private breakpointObserver: BreakpointObserver,
-              public elementHTML: ElementRef,
-              private route: ActivatedRoute) {
-  }
+  constructor(
+    private router: Router,
+    private breakpointObserver: BreakpointObserver,
+    public elementHTML: ElementRef,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    window.addEventListener('scroll', () => this.validatePositionNavBar(), true);
+    // window.addEventListener(
+    //   "scroll",
+    //   () => this.validatePositionNavBar(),
+    //   true
+    // );
   }
 
   /**
    * Redirect to routes of desktop menu
    */
   goOutlet() {
-    this.router.navigate(['./', {outlets: {menuoutlet: 'menu'}}], {
+    this.router.navigate(["./", { outlets: { menuoutlet: "menu" } }], {
       relativeTo: this.route
     });
   }
@@ -52,18 +62,17 @@ export class NavBarComponent implements OnInit, OnDestroy {
    * Fires on profile chip is clicked on desktop devices
    */
   onClickToChip() {
-    console.log('goToProfile');
+    console.log("goToProfile");
   }
 
-  ngOnDestroy(): void {
-  }
+  ngOnDestroy(): void {}
 
   /**
    * Check and active or disable class to set position fixed and
    * generate sticky effect
    */
-  validatePositionNavBar() {
-    const navPosition = this.matToolbar._elementRef.nativeElement.offsetTop + 1;
-    this.activeSticky = window.pageYOffset >= navPosition;
-  }
+  // validatePositionNavBar() {
+  //   const navPosition = this.matToolbar._elementRef.nativeElement.offsetTop + 1;
+  //   this.activeSticky = window.pageYOffset >= navPosition;
+  // }
 }

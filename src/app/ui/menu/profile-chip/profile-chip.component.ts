857fa89c6176915e2e92006a7378d22110118de3
Brayan Nuñez <bnunez@isthmusit.com>
2020-01-06 11:19:29 -0600

    cambios varios

--- src/app/ui/menu/profile-chip/profile-chip.component.ts
+++ src/app/ui/menu/profile-chip/profile-chip.component.ts
@@ -1,20 +1,19 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-profile-chip',
-  templateUrl: './profile-chip.component.html',
-  styleUrls: ['./profile-chip.component.scss']
+  selector: "app-profile-chip",
+  templateUrl: "./profile-chip.component.html",
+  styleUrls: ["./profile-chip.component.scss"]
 })
 export class ProfileChipComponent implements OnInit {
-  @Input() userName = '';
+  @Input() userName = "";
   @Output() goToProfile: EventEmitter<string>;
 
   constructor() {
     this.goToProfile = new EventEmitter<string>();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onChipClick() {
     this.goToProfile.emit(this.userName);
